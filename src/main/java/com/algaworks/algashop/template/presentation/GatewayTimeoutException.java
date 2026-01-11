f37a48c7f4f2ad8fd9749354222b5c98cf8275a5
Jean Malvessi <jeanmalvessi@gmail.com>
2026-01-11 17:39:28 -0300

    Microservice template

---
+++ src/main/java/com/algaworks/algashop/template/presentation/GatewayTimeoutException.java
@@ -0,0 +1,11 @@
+package com.algaworks.algashop.template.presentation;
+
+public class GatewayTimeoutException extends RuntimeException {
+    
+    public GatewayTimeoutException() {
+    }
+    
+    public GatewayTimeoutException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
